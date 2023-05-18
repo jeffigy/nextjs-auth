00c032af9960ad32e66a7c038d055fdd56727983
jeffigy <evangelistajeffy25@gmail.com>
2023-05-18 08:18:10 +0800

    server-side auth

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,8 +1,27 @@
+import { getSession } from "next-auth/react";
 import React from "react";
 
-type blogProps = {};
+type blogProps = {
+  data: any;
+};
 
-const blog: React.FC<blogProps> = () => {
-  return <div>blog page</div>;
+const blog: React.FC<blogProps> = ({ data }) => {
+  return (
+    <div>
+      <h1>blog page</h1>
+      <h4>{data}</h4>
+    </div>
+  );
 };
 export default blog;
+
+export async function getServerSideProps(context: any) {
+  const session = await getSession(context);
+  return {
+    props: {
+      data: session
+        ? "list of 100 personalized blog posts"
+        : "list of 100 non-personalized blog posts",
+    },
+  };
+}

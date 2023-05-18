d88aad3f3e759c51d785423125b4c16b236a0e70
jeffigy <evangelistajeffy25@gmail.com>
2023-05-18 08:04:48 +0800

    end of client-side auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
+import { useSession } from "next-auth/react";
+
 export default function Home() {
+  const { data: session, status } = useSession();
+  console.log(`status: ${status}, session: ${session}`);
   return <>The quick brown fox jumps over the lazy dog.</>;
 }
